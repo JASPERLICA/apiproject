b12f0712591021047d9380b026b500a46148b11b
jasperlica <jasperlica@gmail.com>
2023-10-06 16:52:11 -0400

    new prject for api study

---
+++ apiproject/apiapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework import routers
+from .import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+# 使用自动路由 URL
+# 还有登录 URL
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
